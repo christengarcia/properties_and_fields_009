561988cf46e1d85e4b7d21267077959cbf58a5f8
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-20 21:56:19 +1100

    First commit

---
+++ properties_and_fields_009/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace properties_and_fields_009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
